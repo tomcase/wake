9e999bce6be21fe0521c0cb4ab7e711fa4132332
Tom Case <tomcase77@gmail.com>
2021-06-25 17:57:59 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WakeOnLan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
